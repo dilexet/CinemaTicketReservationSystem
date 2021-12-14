c2ee162f14d1d9f740387f7741819c033d78e2c8
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-14 12:00:21 +0300

    Added cookie model

---
+++ CinemaTicketReservationSystem.WebApi/Cookie/CookieOptions.cs
@@ -0,0 +1,8 @@
+namespace CinemaTicketReservationSystem.WebApi.Cookie
+{
+    public class CookieOptions
+    {
+        public string Name { get; set; }
+        public int LifeTime { get; set; }
+    }
+}
