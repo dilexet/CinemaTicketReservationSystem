448644a219ed7c13cad947ebe6b6f4112be1a926
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-14 12:01:36 +0300

    Configured cookie and jwt, added authenticate controller

--- CinemaTicketReservationSystem.WebApi/Startup.cs
+++ CinemaTicketReservationSystem.WebApi/Startup.cs
@@ -10,7 +10,14 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
+using CinemaTicketReservationSystem.WebApi.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CinemaTicketReservationSystem.WebApi
 {
@@ -25,11 +32,58 @@ namespace CinemaTicketReservationSystem.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            
+            services.AddOptions();
+            services.Configure<JwtOptions>(Configuration.GetSection("JwtOptions"));
+            services.Configure<CinemaTicketReservationSystem.WebApi.Cookie.CookieOptions>(Configuration.GetSection("CookieOptions"));
+            services.AddScoped<JwtService>();
+            services.AddScoped<IdentityService>();
+            
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.Audience = Configuration["JwtOptions:Audience"];
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateLifetime = true,
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["JwtOptions:Key"])),
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidIssuer = Configuration["JwtOptions:Issuer"]
+                    };
+                });
+            services.AddAuthorization();
+            services.AddSwaggerGen(swagger =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CinemaTicketReservationSystem.WebApi", Version = "v1" });
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "CinemaTicketReservationSystem.WebApi", Version = "v1" });
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter ‘Bearer’ [space] and then your valid token",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
             });
         }
 
@@ -39,19 +93,34 @@ namespace CinemaTicketReservationSystem.WebApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CinemaTicketReservationSystem.WebApi v1"));
+                app.UseSwaggerUI(c =>
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CinemaTicketReservationSystem.WebApi v1"));
             }
-
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
-
             app.UseRouting();
+            
+            // TODO: move to middleware
+            app.Use(async (context, next) =>
+            {
+                var token = context.Request.Cookies[Configuration["CookieOptions:Name"]];
+                if (!string.IsNullOrEmpty(token))
+                    context.Request.Headers.Add("Authorization", "Bearer " + token);
 
+                await next();
+            });
+            
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.Strict,
+                HttpOnly = HttpOnlyPolicy.Always,
+                Secure = CookieSecurePolicy.Always
+            });
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
