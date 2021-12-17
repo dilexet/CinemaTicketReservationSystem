607ca94c61df4a481e4db8771e269218f288f2ce
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-17 14:38:59 +0300

    Updated login model request

--- CinemaTicketReservationSystem.WebApi/Models/Requests/Authorize/UserLoginRequest.cs
+++ CinemaTicketReservationSystem.WebApi/Models/Requests/Authorize/UserLoginRequest.cs
@@ -6,7 +6,7 @@ namespace CinemaTicketReservationSystem.WebApi.Models.Requests.Authorize
     public class UserLoginRequest
     {
         [Required(ErrorMessage = "User Name is required")]
-        public String Username { get; set; }
+        public String Name { get; set; }
 
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Password is required")]
