45329df9553f8697b077da844f904c9a24b91e11
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-20 13:35:54 +0300

    refactor code with stylecop

--- CinemaTicketReservationSystem.DAL/Entity/User.cs
+++ CinemaTicketReservationSystem.DAL/Entity/User.cs
@@ -6,11 +6,17 @@ namespace CinemaTicketReservationSystem.DAL.Entity
     public class User
     {
         public Guid Id { get; set; }
-        public String Name { get; set; }
-        public String Email { get; set; }
-        public String PasswordHash { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
         public Guid RoleId { get; set; }
+
         public Role Role { get; set; }
+
         public IEnumerable<RefreshToken> RefreshTokens { get; set; }
     }
 }
