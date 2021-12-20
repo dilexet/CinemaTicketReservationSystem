fb04fcfc9b1221e6496979faae5a3362af685990
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-20 15:33:42 +0300

    Removed validation fields

--- CinemaTicketReservationSystem.WebApi/Controllers/AuthorizeController.cs
+++ CinemaTicketReservationSystem.WebApi/Controllers/AuthorizeController.cs
@@ -5,7 +5,6 @@ using CinemaTicketReservationSystem.BLL.Results;
 using CinemaTicketReservationSystem.WebApi.Models.Requests.Authorize;
 using CinemaTicketReservationSystem.WebApi.Models.Requests.Token;
 using CinemaTicketReservationSystem.WebApi.Models.Response;
-using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -18,75 +17,47 @@ namespace CinemaTicketReservationSystem.WebApi.Controllers
     public class AuthorizeController : ControllerBase
     {
         private readonly IAuthorizeService _authorizeService;
-        private readonly IValidator<UserLoginRequest> _loginValidator;
-        private readonly IValidator<UserRegisterRequest> _registerValidator;
-        private readonly IValidator<RefreshTokenRequest> _refreshTokenValidator;
         private readonly IMapper _mapper;
 
-        public AuthorizeController(IAuthorizeService authorizeService, IMapper mapper, IValidator<UserLoginRequest> loginValidator, IValidator<UserRegisterRequest> registerValidator, IValidator<RefreshTokenRequest> refreshTokenValidator)
+        public AuthorizeController(IAuthorizeService authorizeService, IMapper mapper)
         {
             _authorizeService = authorizeService;
             _mapper = mapper;
-            _loginValidator = loginValidator;
-            _registerValidator = registerValidator;
-            _refreshTokenValidator = refreshTokenValidator;
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserLoginRequest userLoginRequest)
         {
-            var validatorResult = await _loginValidator.ValidateAsync(userLoginRequest);
-            if (!validatorResult.IsValid)
-            {
-                return BadRequest(new AuthorizeResponse()
-                {
-                    Code = StatusCodes.Status500InternalServerError,
-                    Success = false,
-                    Errors = validatorResult.Errors
-                });
-            }
+           AuthorizeResult result;
+           try
+           {
+               result = await _authorizeService.LoginAsync(_mapper.Map<LoginModel>(userLoginRequest));
+           }
+           catch (Exception e)
+           {
+               return BadRequest(new AuthorizeResponse()
+               {
+                   Code = StatusCodes.Status400BadRequest,
+                   Success = false,
+                   Errors = new[] { e.Message }
+               });
+           }
 
-            AuthorizeResult result;
-            try
-            {
-                result = await _authorizeService.LoginAsync(_mapper.Map<LoginModel>(userLoginRequest));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(new AuthorizeResponse()
-                {
-                    Code = StatusCodes.Status500InternalServerError,
-                    Success = false,
-                    Errors = new[] { e.Message }
-                });
-            }
+           var response = _mapper.Map<AuthorizeResponse>(result);
 
-            AuthorizeResponse response = _mapper.Map<AuthorizeResponse>(result);
+           if (response.Success)
+           {
+               response.Code = StatusCodes.Status200OK;
+               return Ok(response);
+           }
 
-            if (response.Success)
-            {
-                response.Code = StatusCodes.Status200OK;
-                return Ok(response);
-            }
-
-            response.Code = StatusCodes.Status401Unauthorized;
-            return Unauthorized(response);
+           response.Code = StatusCodes.Status401Unauthorized;
+           return Unauthorized(response);
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserRegisterRequest userRegisterRequest)
         {
-            var validatorResult = await _registerValidator.ValidateAsync(userRegisterRequest);
-            if (!validatorResult.IsValid)
-            {
-                return BadRequest(new AuthorizeResponse()
-                {
-                    Code = StatusCodes.Status500InternalServerError,
-                    Success = false,
-                    Errors = validatorResult.Errors
-                });
-            }
-
             AuthorizeResult result;
             try
             {
@@ -96,7 +67,7 @@ namespace CinemaTicketReservationSystem.WebApi.Controllers
             {
                 return BadRequest(new AuthorizeResponse()
                 {
-                    Code = StatusCodes.Status500InternalServerError,
+                    Code = StatusCodes.Status400BadRequest,
                     Success = false,
                     Errors = new[] { e.Message }
                 });
@@ -117,17 +88,6 @@ namespace CinemaTicketReservationSystem.WebApi.Controllers
         [HttpPost("refresh")]
         public async Task<IActionResult> RefreshToken(RefreshTokenRequest refreshTokenRequest)
         {
-            var validatorResult = await _refreshTokenValidator.ValidateAsync(refreshTokenRequest);
-            if (!validatorResult.IsValid)
-            {
-                return BadRequest(new AuthorizeResponse()
-                {
-                    Code = StatusCodes.Status500InternalServerError,
-                    Success = false,
-                    Errors = validatorResult.Errors
-                });
-            }
-
             AuthorizeResult result;
             try
             {
@@ -139,7 +99,7 @@ namespace CinemaTicketReservationSystem.WebApi.Controllers
             {
                 return BadRequest(new AuthorizeResponse()
                 {
-                    Code = StatusCodes.Status500InternalServerError,
+                    Code = StatusCodes.Status400BadRequest,
                     Success = false,
                     Errors = new[] { e.Message }
                 });
