b8bbbed3592c3a4b8cf7c8a4ed54a7c90e927579
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-21 17:25:32 +0300

    Added models to user management

---
+++ CinemaTicketReservationSystem.WebApi/Models/Requests/User/UserCreateRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Requests.User
+{
+    public class UserCreateRequest
+    {
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string RoleName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ CinemaTicketReservationSystem.WebApi/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaTicketReservationSystem.WebApi.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string RoleName { get; set; }
+    }
+}
