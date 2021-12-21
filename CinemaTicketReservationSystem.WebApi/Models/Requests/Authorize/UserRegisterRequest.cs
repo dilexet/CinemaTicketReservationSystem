55189c950f47b30badc5f7fadcbf3ae2bb6c8643
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-21 17:24:55 +0300

    Added [EmailAddress] attribute to this model

--- CinemaTicketReservationSystem.WebApi/Models/Requests/Authorize/UserRegisterRequest.cs
+++ CinemaTicketReservationSystem.WebApi/Models/Requests/Authorize/UserRegisterRequest.cs
@@ -1,9 +1,12 @@
-﻿namespace CinemaTicketReservationSystem.WebApi.Models.Requests.Authorize
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Requests.Authorize
 {
     public class UserRegisterRequest
     {
         public string Name { get; set; }
 
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Password { get; set; }
