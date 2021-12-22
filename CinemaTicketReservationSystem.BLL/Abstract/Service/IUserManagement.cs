955a8b2e91bd5b05a808ccceed98a164bbf587b8
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-22 16:20:52 +0300

    Added user management service

---
+++ CinemaTicketReservationSystem.BLL/Abstract/Service/IUserManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using CinemaTicketReservationSystem.BLL.Domain.UserModels;
+using CinemaTicketReservationSystem.BLL.Results;
+
+namespace CinemaTicketReservationSystem.BLL.Abstract.Service
+{
+    public interface IUserManagement
+    {
+        Task<UserManagementGetUsersResult> GetUsers();
+
+        Task<UserManagementResult> CreateUser(UserModel userModel);
+
+        Task<UserManagementResult> UpdateUser(Guid id, UserModel userModel);
+
+        Task<UserManagementRemoveResult> DeleteUser(Guid id);
+    }
+}
--- CinemaTicketReservationSystem.BLL/Services/UserManagement.cs
+++ CinemaTicketReservationSystem.BLL/Services/UserManagement.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
-using CinemaTicketReservationSystem.BLL.Abstract;
+using CinemaTicketReservationSystem.BLL.Abstract.Service;
 using CinemaTicketReservationSystem.BLL.Domain.UserModels;
+using CinemaTicketReservationSystem.BLL.Results;
 using CinemaTicketReservationSystem.DAL.Abstract;
 using CinemaTicketReservationSystem.DAL.Entity;
 using Microsoft.EntityFrameworkCore;
@@ -22,56 +24,201 @@ namespace CinemaTicketReservationSystem.BLL.Services
             _mapper = mapper;
         }
 
-        public async Task<IEnumerable<UserModel>> GetUsers()
+        public async Task<UserManagementGetUsersResult> GetUsers()
         {
             var users = await _userRepository.GetBy().ToListAsync();
-            return _mapper.Map<IEnumerable<UserModel>>(users);
-        }
 
-        public async Task<UserModel> CreateUser(UserModel userModel)
-        {
-            var user = _mapper.Map<User>(userModel);
-            user.PasswordHash = _userRepository.HashPassword(userModel.Password);
-            if (!await _userRepository.CreateAsync(user))
+            IEnumerable<UserModel> usersModel;
+            try
             {
-                return null;
+                usersModel = _mapper.Map<IEnumerable<UserModel>>(users);
+            }
+            catch (AutoMapperMappingException e)
+            {
+                return new UserManagementGetUsersResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        e.Message
+                    }
+                };
             }
 
+            return new UserManagementGetUsersResult()
+            {
+                Success = true,
+                UserModels = usersModel
+            };
+        }
+
+        public async Task<UserManagementResult> CreateUser(UserModel userModel)
+        {
+            var userExist = await _userRepository.FirstOrDefaultAsync(x => x.Name == userModel.Name);
+            if (userExist != null)
+            {
+                return new UserManagementResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "User is exists"
+                    }
+                };
+            }
+
+            var user = _mapper.Map<User>(userModel);
+            user.PasswordHash = _userRepository.HashPassword(userModel.Password);
             var role = await _roleRepository.FirstOrDefaultAsync(role => role.Name == userModel.RoleModel.Name);
             if (role == null)
             {
-                return null;
+                return new UserManagementResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "Role is not exists"
+                    }
+                };
             }
 
             user.Role = role;
-            if (await _userRepository.UpdateAsync(user))
+            if (!await _userRepository.CreateAsync(user))
             {
-                return userModel;
+                return new UserManagementResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "An error occured while adding to the database"
+                    }
+                };
             }
 
-            return null;
+            UserModel newUserModel;
+            try
+            {
+                newUserModel = _mapper.Map<UserModel>(user);
+            }
+            catch (AutoMapperMappingException e)
+            {
+                return new UserManagementResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        e.Message
+                    }
+                };
+            }
+
+            return new UserManagementResult()
+            {
+                Success = true,
+                UserModel = newUserModel
+            };
         }
 
-        public async Task<UserModel> UpdateUser(UserModel userModel)
+        public async Task<UserManagementResult> UpdateUser(Guid id, UserModel userModel)
         {
-            var userExist = await _userRepository.FirstOrDefaultAsync(user => user.Name == userModel.Name);
+            var userExist = await _userRepository.FindByIdAsync(id);
             if (userExist == null)
             {
-                return null;
+                return new UserManagementResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "User is not exists"
+                    }
+                };
             }
 
-            var result = await _userRepository.UpdateAsync(userExist);
-            if (result)
+            var roleExist = await _roleRepository.FirstOrDefaultAsync(role => role.Name == userModel.RoleModel.Name);
+            if (roleExist == null)
             {
-                return userModel;
+                return new UserManagementResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "Role is not exists"
+                    }
+                };
             }
 
-            return null;
+            userExist.Name = userModel.Name;
+            userExist.Email = userModel.Email;
+            userExist.Role = roleExist;
+
+            if (!await _userRepository.UpdateAsync(userExist))
+            {
+                return new UserManagementResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "An error occured while updating to the database"
+                    }
+                };
+            }
+
+            UserModel newUserModel;
+            try
+            {
+                newUserModel = _mapper.Map<UserModel>(userExist);
+            }
+            catch (AutoMapperMappingException e)
+            {
+                return new UserManagementResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        e.Message
+                    }
+                };
+            }
+
+            return new UserManagementResult()
+            {
+                Success = true,
+                UserModel = newUserModel
+            };
         }
 
-        public Task<UserModel> DeleteUser(UserModel userModel)
+        public async Task<UserManagementRemoveResult> DeleteUser(Guid id)
         {
-            throw new System.NotImplementedException();
+            var userExist = await _userRepository.FindByIdAsync(id);
+            if (userExist == null)
+            {
+                return new UserManagementRemoveResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "User is not exists"
+                    }
+                };
+            }
+
+            if (!await _userRepository.RemoveAsync(userExist))
+            {
+                return new UserManagementRemoveResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "An error occured while removing to the database"
+                    }
+                };
+            }
+
+            return new UserManagementRemoveResult()
+            {
+                Success = true,
+                Id = id
+            };
         }
     }
 }
