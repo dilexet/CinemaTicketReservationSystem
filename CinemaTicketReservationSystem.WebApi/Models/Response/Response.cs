bc7d6ffd6a452bde4befea50f00a165460c6b759
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-22 16:19:06 +0300

    Added generic class for response

---
+++ CinemaTicketReservationSystem.WebApi/Models/Response/Response.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Response
+{
+    public class Response
+    {
+        public int Code { get; set; }
+
+        public bool Success { get; set; }
+
+        public IEnumerable<object> Errors { get; set; }
+    }
+}
