1bea24bb4141491a0e9157621b917717baa5d2a0
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-22 16:20:00 +0300

    Added result models for user management

---
+++ CinemaTicketReservationSystem.BLL/Results/UserManagementResult.cs
@@ -0,0 +1,9 @@
+﻿using CinemaTicketReservationSystem.BLL.Domain.UserModels;
+
+namespace CinemaTicketReservationSystem.BLL.Results
+{
+    public class UserManagementResult : Result
+    {
+        public UserModel UserModel { get; set; }
+    }
+}
