c2762d1e17d0819cca551b8e9b50365e681ddc69
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-22 16:22:03 +0300

    Changed user register request model

--- CinemaTicketReservationSystem.WebApi/Models/Requests/Authorize/UserRegisterRequest.cs
+++ CinemaTicketReservationSystem.WebApi/Models/Requests/Authorize/UserRegisterRequest.cs
@@ -1,12 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace CinemaTicketReservationSystem.WebApi.Models.Requests.Authorize
+﻿namespace CinemaTicketReservationSystem.WebApi.Models.Requests.Authorize
 {
     public class UserRegisterRequest
     {
         public string Name { get; set; }
 
-        [EmailAddress]
         public string Email { get; set; }
 
         public string Password { get; set; }
