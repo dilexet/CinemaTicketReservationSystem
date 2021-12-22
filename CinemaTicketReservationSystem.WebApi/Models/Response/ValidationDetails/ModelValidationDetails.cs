79cb1d3856cc77a730fa385752c96a2098aa4906
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-22 18:40:56 +0300

    Added validation details model

---
+++ CinemaTicketReservationSystem.WebApi/Models/Response/ValidationDetails/ModelValidationDetails.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Response.ValidationDetails
+{
+    public class ModelValidationDetails
+    {
+        public string Field { get; set; }
+
+        public IEnumerable<object> Errors { get; set; }
+    }
+}
