f319e282aeaedc04d8ac2104a67ebdc0a283dc5d
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-22 16:23:02 +0300

    Changed authorize response

--- CinemaTicketReservationSystem.WebApi/Models/Response/AuthorizeResponse.cs
+++ CinemaTicketReservationSystem.WebApi/Models/Response/AuthorizeResponse.cs
@@ -1,17 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace CinemaTicketReservationSystem.WebApi.Models.Response
+﻿namespace CinemaTicketReservationSystem.WebApi.Models.Response
 {
-    public class AuthorizeResponse
+    public class AuthorizeResponse : Response
     {
-        public int Code { get; set; }
-
-        public bool Success { get; set; }
-
         public string JwtToken { get; set; }
 
         public string RefreshToken { get; set; }
-
-        public IEnumerable<object> Errors { get; set; }
     }
 }
