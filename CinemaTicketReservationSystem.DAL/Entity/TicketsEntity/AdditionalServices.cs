c151483227b55a0e98deb94ba1afc7c0383014a9
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-23 16:35:09 +0300

    Added ticket entities

---
+++ CinemaTicketReservationSystem.DAL/Entity/TicketsEntity/AdditionalServices.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaTicketReservationSystem.DAL.Entity.TicketsEntity
+{
+    public class AdditionalServices : BasedEntity
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ CinemaTicketReservationSystem.DAL/Entity/TicketsEntity/Ticket.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CinemaTicketReservationSystem.DAL.Entity.CinemaEntity;
+
+namespace CinemaTicketReservationSystem.DAL.Entity.TicketsEntity
+{
+    public class Ticket : BasedEntity
+    {
+        public Seat Seat { get; set; }
+
+        public IEnumerable<AdditionalServices> AdditionalServices { get; set; }
+    }
+}
