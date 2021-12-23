1ac49083e5c73ff917b4a48fe134253b12592d02
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-23 16:34:47 +0300

    Added movie entities

---
+++ CinemaTicketReservationSystem.DAL/Entity/MovieEntity/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CinemaTicketReservationSystem.DAL.Entity.MovieEntity
+{
+    public class Movie : BasedEntity
+    {
+        public string Name { get; set; }
+
+        public MovieDescription MovieDescription { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ CinemaTicketReservationSystem.DAL/Entity/MovieEntity/MovieDescription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CinemaTicketReservationSystem.DAL.Entity.MovieEntity
+{
+    public class MovieDescription : BasedEntity
+    {
+        public DateTime ReleaseDate { get; set; }
+
+        public string Description { get; set; }
+
+        public IEnumerable<string> Countries { get; set; }
+
+        public IEnumerable<string> Genres { get; set; }
+
+        public IEnumerable<string> Directors { get; set; }
+
+        public IEnumerable<string> Screenwriters { get; set; }
+
+        public IEnumerable<string> Producers { get; set; }
+
+        public IEnumerable<string> Actors { get; set; }
+    }
+}
