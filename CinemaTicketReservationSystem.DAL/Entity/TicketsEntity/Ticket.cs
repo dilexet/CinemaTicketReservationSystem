e7eda2e474d0040b378c6baee3dc17984d147c43
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-24 09:15:47 +0300

    changed ticket entity

--- CinemaTicketReservationSystem.DAL/Entity/TicketsEntity/Ticket.cs
+++ CinemaTicketReservationSystem.DAL/Entity/TicketsEntity/Ticket.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections.Generic;
 using CinemaTicketReservationSystem.DAL.Entity.CinemaEntity;
+using CinemaTicketReservationSystem.DAL.Entity.SessionEntity;
 
 namespace CinemaTicketReservationSystem.DAL.Entity.TicketsEntity
 {
     public class Ticket : BasedEntity
     {
+        public decimal TotalPrice { get; set; }
+
+        public Session Session { get; set; }
+
         public Seat Seat { get; set; }
 
-        public IEnumerable<AdditionalServices> AdditionalServices { get; set; }
+        public IEnumerable<AdditionalService> AdditionalServices { get; set; }
     }
 }
