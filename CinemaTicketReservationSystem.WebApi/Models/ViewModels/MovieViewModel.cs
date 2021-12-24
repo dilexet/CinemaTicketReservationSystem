d3a3d79a3b32cbd5abd0144bd7919a91b0a2e3cb
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-24 16:15:13 +0300

    Added movie view model

---
+++ CinemaTicketReservationSystem.WebApi/Models/ViewModels/MovieViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.ViewModels
+{
+    public class MovieViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Description { get; set; }
+
+        public IEnumerable<string> Countries { get; set; }
+
+        public IEnumerable<string> Genres { get; set; }
+
+        public IEnumerable<string> Directors { get; set; }
+
+        public IEnumerable<string> Screenwriters { get; set; }
+
+        public IEnumerable<string> Producers { get; set; }
+
+        public IEnumerable<string> Actors { get; set; }
+    }
+}
