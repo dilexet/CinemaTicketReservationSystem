174e47cb34d72b02745ce7337da683cef483dcbf
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-24 16:12:11 +0300

    Added movie service

---
+++ CinemaTicketReservationSystem.BLL/Services/MovieService.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using CinemaTicketReservationSystem.BLL.Abstract.Service;
+using CinemaTicketReservationSystem.BLL.Domain.MovieModels;
+using CinemaTicketReservationSystem.BLL.Results.Movie;
+using CinemaTicketReservationSystem.DAL.Abstract.Movie;
+using CinemaTicketReservationSystem.DAL.Entity.MovieEntity;
+
+namespace CinemaTicketReservationSystem.BLL.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+        private readonly IMapper _mapper;
+
+        public MovieService(IMovieRepository movieRepository, IMapper mapper)
+        {
+            _movieRepository = movieRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<MovieServiceResult> AddMovie(MovieModel movieModel)
+        {
+            var movieExist = await _movieRepository.FirstOrDefaultAsync(x => x.Name.Equals(movieModel));
+            if (movieExist != null)
+            {
+                return new MovieServiceResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "Movie is exists"
+                    }
+                };
+            }
+
+            var movie = _mapper.Map<Movie>(movieModel);
+
+            if (!await _movieRepository.CreateAsync(movie))
+            {
+                return new MovieServiceResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "An error occured while adding to the database"
+                    }
+                };
+            }
+
+            MovieModel newMovieModel = _mapper.Map<MovieModel>(movie);
+
+            return new MovieServiceResult()
+            {
+                Success = true,
+                MovieModel = newMovieModel
+            };
+        }
+    }
+}
