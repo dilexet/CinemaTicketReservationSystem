e205d659496fefcd64afce16879eeadc77e94636
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-24 16:16:08 +0300

    Added extensions

---
+++ CinemaTicketReservationSystem.WebApi/Extensions/AuthenticationConfigureExtension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using CinemaTicketReservationSystem.DAL.Enums;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CinemaTicketReservationSystem.WebApi.Extensions
+{
+    public static class AuthenticationConfigureExtension
+    {
+        public static void AddAuthenticationToConfigureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidIssuer = configuration["JwtOptions:Issuer"],
+                ValidAudience = configuration["JwtOptions:Audience"],
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["JwtOptions:AccessTokenSecret"])),
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true
+            };
+
+            services.AddAuthentication(
+                    option =>
+                    {
+                        option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.Audience = configuration["JwtOptions:Audience"];
+                    options.TokenValidationParameters = tokenValidationParameters;
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminRole", policy =>
+                    policy.RequireRole(RoleTypes.Admin.ToString()));
+                options.AddPolicy("UserRole", policy =>
+                    policy.RequireRole(RoleTypes.User.ToString()));
+                options.AddPolicy("ManagerRole", policy =>
+                    policy.RequireRole(RoleTypes.Manager.ToString()));
+            });
+        }
+    }
+}
---
+++ CinemaTicketReservationSystem.WebApi/Extensions/SwaggerConfigureExtension.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace CinemaTicketReservationSystem.WebApi.Extensions
+{
+    public static class SwaggerConfigureExtension
+    {
+        public static void AddSwaggerToConfigureServices(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Title = "CinemaTicketReservationSystem.WebApi", Version = "v1"
+                    });
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter ‘Bearer’ [space] and then your valid token",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+        }
+
+        public static void UseSwaggerConfigure(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CinemaTicketReservationSystem.WebApi v1"));
+        }
+    }
+}
