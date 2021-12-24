fb23ff4f161210c8938c76f8fe0f9bb29f6262c8
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-24 17:10:19 +0300

    Changed movie view model

--- CinemaTicketReservationSystem.WebApi/Models/ViewModels/MovieViewModel.cs
+++ CinemaTicketReservationSystem.WebApi/Models/ViewModels/MovieViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CinemaTicketReservationSystem.WebApi.Models.ViewModels
 {
@@ -13,20 +12,6 @@ namespace CinemaTicketReservationSystem.WebApi.Models.ViewModels
 
         public DateTime EndDate { get; set; }
 
-        public DateTime ReleaseDate { get; set; }
-
-        public string Description { get; set; }
-
-        public IEnumerable<string> Countries { get; set; }
-
-        public IEnumerable<string> Genres { get; set; }
-
-        public IEnumerable<string> Directors { get; set; }
-
-        public IEnumerable<string> Screenwriters { get; set; }
-
-        public IEnumerable<string> Producers { get; set; }
-
-        public IEnumerable<string> Actors { get; set; }
+        public ModelDescriptionViewModel ModelDescriptionViewModel { get; set; }
     }
 }
