97349986c58f8b3f8218fffbbcb2c52158f08fee
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-24 16:12:54 +0300

    Added movie result model

---
+++ CinemaTicketReservationSystem.BLL/Results/Movie/MovieServiceResult.cs
@@ -0,0 +1,9 @@
+﻿using CinemaTicketReservationSystem.BLL.Domain.MovieModels;
+
+namespace CinemaTicketReservationSystem.BLL.Results.Movie
+{
+    public class MovieServiceResult : Result
+    {
+        public MovieModel MovieModel { get; set; }
+    }
+}
