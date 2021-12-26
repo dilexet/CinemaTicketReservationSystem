4ab53a0249b1d5682aefd4e697ade7cf267adfc7
Dilexet <dilexet@gmail.com>
2021-12-26 18:14:12 +0300

    Added movie request validator

---
+++ CinemaTicketReservationSystem.WebApi/Validators/MovieRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using CinemaTicketReservationSystem.WebApi.Models.Requests.Movie;
+using FluentValidation;
+
+namespace CinemaTicketReservationSystem.WebApi.Validators
+{
+    public class MovieRequestValidator : AbstractValidator<MovieRequest>
+    {
+        // TODO: add parameters
+        public MovieRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Movie name can't be empty");
+
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description can't be empty");
+            RuleFor(x => x.Countries).NotNull().NotEmpty().WithMessage("Countries can't be empty");
+            RuleFor(x => x.Genres).NotNull().NotEmpty().WithMessage("Genres can't be empty");
+        }
+    }
+}
