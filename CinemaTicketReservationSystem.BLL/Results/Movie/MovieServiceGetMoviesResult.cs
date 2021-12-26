474b2d276d5251f84ccc5c88437b7ed7ced942a2
Dilexet <dilexet@gmail.com>
2021-12-26 22:19:51 +0300

    added movie service get all model

---
+++ CinemaTicketReservationSystem.BLL/Results/Movie/MovieServiceGetMoviesResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CinemaTicketReservationSystem.BLL.Domain.MovieModels;
+
+namespace CinemaTicketReservationSystem.BLL.Results.Movie
+{
+    public class MovieServiceGetMoviesResult : Result
+    {
+        public IEnumerable<MovieModel> MovieModels { get; set; }
+    }
+}
