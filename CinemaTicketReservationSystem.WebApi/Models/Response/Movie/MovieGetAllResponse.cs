53d2ca330b5ac2cbfb60221930d1561ba03fb906
Dilexet <dilexet@gmail.com>
2021-12-26 22:20:21 +0300

    added movie gel all response

---
+++ CinemaTicketReservationSystem.WebApi/Models/Response/Movie/MovieGetAllResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CinemaTicketReservationSystem.WebApi.Models.ViewModels;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Response.Movie
+{
+    public class MovieGetAllResponse : Response
+    {
+        public IEnumerable<MovieViewModel> Movies { get; set; }
+    }
+}
