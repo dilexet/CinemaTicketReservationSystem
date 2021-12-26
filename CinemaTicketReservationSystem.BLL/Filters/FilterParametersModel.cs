5db189876a1aeeacfac6cd7f0bfd582698321417
Dilexet <dilexet@gmail.com>
2021-12-26 22:19:32 +0300

    Add filter model

---
+++ CinemaTicketReservationSystem.BLL/Filters/FilterParametersModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaTicketReservationSystem.BLL.Filters
+{
+    public class FilterParametersModel
+    {
+        public string SearchQuery { get; set; }
+
+        public string SortBy { get; set; }
+    }
+}
