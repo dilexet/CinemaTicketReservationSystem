5ac243378d639b6a00c6a66c60336a0245ed5a3b
Dilexet <dilexet@gmail.com>
2021-12-26 18:32:41 +0300

    Added movie remove response

---
+++ CinemaTicketReservationSystem.WebApi/Models/Response/Movie/MovieRemoveResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Response.Movie
+{
+    public class MovieRemoveResponse : Response
+    {
+        public Guid Id { get; set; }
+    }
+}
