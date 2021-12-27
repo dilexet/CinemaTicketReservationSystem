b41ffc8694d2b4da0962bc68f1113c7a3f5698a7
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-27 13:00:19 +0300

    Added abstract cinema service

--- CinemaTicketReservationSystem.BLL/Abstract/Service/ICinemaService.cs
+++ CinemaTicketReservationSystem.BLL/Abstract/Service/ICinemaService.cs
@@ -1,6 +1,20 @@
-﻿namespace CinemaTicketReservationSystem.BLL.Abstract.Service
+﻿using System;
+using System.Threading.Tasks;
+using CinemaTicketReservationSystem.BLL.Domain.CinemaModels;
+using CinemaTicketReservationSystem.BLL.Results.Cinema;
+
+namespace CinemaTicketReservationSystem.BLL.Abstract.Service
 {
     public interface ICinemaService
     {
+        Task<CinemaServiceResult> AddCinema(CinemaModel cinema);
+
+        Task<CinemaServiceResult> UpdateCinemaInfo(Guid id, CinemaModel cinema);
+
+        Task<CinemaServiceRemoveResult> RemoveCinema(Guid id);
+
+        Task<CinemaServiceGetAllResult> GetCinemas();
+
+        Task<CinemaServiceResult> GetCinemaById(Guid id);
     }
 }
