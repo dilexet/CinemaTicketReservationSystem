8caa151eeea627fd8fec8398708e537c1bca693d
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-27 17:01:07 +0300

    Added cinema response

---
+++ CinemaTicketReservationSystem.WebApi/Models/Response/Cinema/CinemaGetAllResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CinemaTicketReservationSystem.WebApi.Models.ViewModels.Cinema;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Response.Cinema
+{
+    public class CinemaGetAllResponse : Response
+    {
+        public IEnumerable<CinemaViewModel> Cinemas { get; set; }
+    }
+}
