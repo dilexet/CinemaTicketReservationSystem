47126094375ae9d9abfb4a4b59a4f496a5e72aa4
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-27 12:56:01 +0300

    Added address entity

---
+++ CinemaTicketReservationSystem.DAL/Entity/CinemaEntity/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaTicketReservationSystem.DAL.Entity.CinemaEntity
+{
+    public class Address : BasedEntity
+    {
+        public string CityName { get; set; }
+
+        public string Street { get; set; }
+
+        public virtual Cinema Cinema { get; set; }
+    }
+}
