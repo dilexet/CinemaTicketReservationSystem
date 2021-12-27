b1178a8eb7e719d0415c327895beca275680b280
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-27 17:07:26 +0300

    Added role types

--- CinemaTicketReservationSystem.DAL/EntityConfiguration/AuthorizeConfigurations/RoleConfiguration.cs
+++ CinemaTicketReservationSystem.DAL/EntityConfiguration/AuthorizeConfigurations/RoleConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CinemaTicketReservationSystem.DAL.Entity.AuthorizeEntity;
+using CinemaTicketReservationSystem.DAL.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -19,15 +20,15 @@ namespace CinemaTicketReservationSystem.DAL.EntityConfiguration.AuthorizeConfigu
             builder.HasData(
                 new
                 {
-                    Id = Guid.NewGuid(), Name = "Admin"
+                    Id = Guid.NewGuid(), Name = RoleTypes.Admin.ToString()
                 },
                 new
                 {
-                    Id = Guid.NewGuid(), Name = "Manager"
+                    Id = Guid.NewGuid(), Name = RoleTypes.Manager.ToString()
                 },
                 new
                 {
-                    Id = Guid.NewGuid(), Name = "User"
+                    Id = Guid.NewGuid(), Name = RoleTypes.User.ToString()
                 });
         }
     }
