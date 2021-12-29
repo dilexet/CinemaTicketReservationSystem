491269af8ad71d788f2cdc75fdccc520f373c5cb
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-29 12:49:36 +0300

    Added hall controller

---
+++ CinemaTicketReservationSystem.WebApi/Controllers/HallController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using CinemaTicketReservationSystem.BLL.Abstract.Service;
+using CinemaTicketReservationSystem.BLL.Domain.HallModels;
+using CinemaTicketReservationSystem.WebApi.Models.Requests.Cinema;
+using CinemaTicketReservationSystem.WebApi.Models.Response.Hall;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaTicketReservationSystem.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    // [Authorize(Policy = "AdminRole")]
+    [ApiController]
+    public class HallController : ControllerBase
+    {
+        private readonly IHallService _hallService;
+        private readonly IMapper _mapper;
+
+        public HallController(IHallService hallService, IMapper mapper)
+        {
+            _hallService = hallService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddHall(
+            Guid cinemaId, HallRequest hallRequest)
+        {
+            var hallResult = await _hallService.AddHall(
+                cinemaId,
+                _mapper.Map<HallModel>(hallRequest));
+
+            var response = _mapper.Map<HallResponse>(hallResult);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status400BadRequest;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateHall(Guid id, HallRequest hallRequest)
+        {
+            var hallResult = await _hallService.UpdateHall(
+                id,
+                _mapper.Map<HallModel>(hallRequest));
+
+            var response = _mapper.Map<HallResponse>(hallResult);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status400BadRequest;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveCinema(Guid id)
+        {
+            var hallResult = await _hallService.RemoveHall(id);
+            var response = _mapper.Map<HallRemoveResponse>(hallResult);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status400BadRequest;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCinemaById(Guid id)
+        {
+            var hallResult = await _hallService.GetHallById(id);
+            var response = _mapper.Map<HallResponse>(hallResult);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status404NotFound;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCinemas()
+        {
+            var hallResult = await _hallService.GetHalls();
+            var response = _mapper.Map<HallGetAllResponse>(hallResult);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status404NotFound;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+    }
+}
