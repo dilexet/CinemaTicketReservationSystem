fc0356e3eb734a57cd486b6cc391473ccd5ac9d2
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-29 12:48:00 +0300

    Removed number of halls field

--- CinemaTicketReservationSystem.WebApi/Models/Requests/Cinema/CinemaRequest.cs
+++ CinemaTicketReservationSystem.WebApi/Models/Requests/Cinema/CinemaRequest.cs
@@ -6,8 +6,6 @@ namespace CinemaTicketReservationSystem.WebApi.Models.Requests.Cinema
     {
         public string Name { get; set; }
 
-        public uint NumberOfHalls { get; set; }
-
         public string CityName { get; set; }
 
         public string Street { get; set; }
