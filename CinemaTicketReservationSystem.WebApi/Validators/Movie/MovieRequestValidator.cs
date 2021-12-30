3de5f3e6957a23abf483e17bab099dce7a5538cd
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-30 09:19:09 +0300

    Added url validate

--- CinemaTicketReservationSystem.WebApi/Validators/Movie/MovieRequestValidator.cs
+++ CinemaTicketReservationSystem.WebApi/Validators/Movie/MovieRequestValidator.cs
@@ -1,4 +1,5 @@
-﻿using CinemaTicketReservationSystem.WebApi.Models.Requests.Movie;
+﻿using System;
+using CinemaTicketReservationSystem.WebApi.Models.Requests.Movie;
 using FluentValidation;
 
 namespace CinemaTicketReservationSystem.WebApi.Validators.Movie
@@ -8,6 +9,13 @@ namespace CinemaTicketReservationSystem.WebApi.Validators.Movie
         public MovieRequestValidator()
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("Movie name can't be empty");
+
+            RuleFor(x => x.PosterUrl).NotEmpty().WithMessage("Poster url can't be empty");
+            RuleFor(x => x.PosterUrl)
+                .Must(url => Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+                             && (uriResult.Scheme == Uri.UriSchemeHttp ||
+                                 uriResult.Scheme == Uri.UriSchemeHttps))
+                .WithMessage("Poster url doesn't match URL format");
 
             RuleFor(x => x.StartDate).NotNull().NotEmpty().WithMessage("Movie start date can't be empty");
             RuleFor(x => x.EndDate).NotNull().NotEmpty().WithMessage("Movie end date can't be empty");
