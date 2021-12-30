4fb1709e9fed1cb4398f74b66fceeec53b6fcee6
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-30 15:02:16 +0300

    Added session model request validator

---
+++ CinemaTicketReservationSystem.WebApi/Validators/Session/SessionAdditionalServicesRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using CinemaTicketReservationSystem.WebApi.Models.Requests.Session;
+using FluentValidation;
+
+namespace CinemaTicketReservationSystem.WebApi.Validators.Session
+{
+    public class SessionAdditionalServicesRequestValidator : AbstractValidator<SessionAdditionalServiceRequest>
+    {
+        public SessionAdditionalServicesRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Session additional service name can't be empty");
+
+            RuleFor(x => x.Price).NotEmpty().NotEqual(0)
+                .WithMessage("Session additional service price can't be empty or equal 0");
+        }
+    }
+}
