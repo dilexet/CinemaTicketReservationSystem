2052c1d4b7c1d23b90e5345b82f6b1b47c08e468
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-30 16:54:10 +0300

    Added method to add session

--- CinemaTicketReservationSystem.BLL/Services/SessionService.cs
+++ CinemaTicketReservationSystem.BLL/Services/SessionService.cs
@@ -1,31 +1,136 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using CinemaTicketReservationSystem.BLL.Abstract.Service;
 using CinemaTicketReservationSystem.BLL.Models.Domain.SessionModels;
 using CinemaTicketReservationSystem.BLL.Models.Results.Session;
 using CinemaTicketReservationSystem.DAL.Abstract;
+using CinemaTicketReservationSystem.DAL.Entity.CinemaEntity;
+using CinemaTicketReservationSystem.DAL.Entity.MovieEntity;
 using CinemaTicketReservationSystem.DAL.Entity.SessionEntity;
+using CinemaTicketReservationSystem.DAL.Enums;
 
 namespace CinemaTicketReservationSystem.BLL.Services
 {
     public class SessionService : ISessionService
     {
         private readonly IRepository<Session> _sessionRepository;
+        private readonly IRepository<Movie> _movieRepository;
+        private readonly IRepository<Cinema> _cinemaRepository;
         private readonly IMapper _mapper;
 
-        public SessionService(IRepository<Session> sessionRepository, IMapper mapper)
+        public SessionService(
+            IRepository<Session> sessionRepository,
+            IRepository<Movie> movieRepository,
+            IRepository<Cinema> cinemaRepository,
+            IMapper mapper)
         {
             _sessionRepository = sessionRepository;
+            _movieRepository = movieRepository;
+            _cinemaRepository = cinemaRepository;
             _mapper = mapper;
         }
 
-        public Task<SessionServiceResult> AddSession(SessionModel sessionModel)
+        public async Task<SessionServiceResult> AddSession(SessionRequestModel sessionModel)
         {
-            throw new NotImplementedException();
+            var cinemaExist =
+                await _cinemaRepository.FirstOrDefaultAsync(cinema => cinema.Name.Equals(sessionModel.CinemaName));
+            if (cinemaExist == null)
+            {
+                return new SessionServiceResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "Cinema is not exists"
+                    }
+                };
+            }
+
+            var hallExist = cinemaExist.Halls.FirstOrDefault(hall => hall.Name.Equals(sessionModel.HallName));
+
+            if (hallExist == null)
+            {
+                return new SessionServiceResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "Hall is not exists in this cinema"
+                    }
+                };
+            }
+
+            var movieExist =
+                await _movieRepository.FirstOrDefaultAsync(movie => movie.Name.Equals(sessionModel.MovieName));
+
+            if (movieExist == null)
+            {
+                return new SessionServiceResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "Movie is not exists"
+                    }
+                };
+            }
+
+            List<SessionAdditionalService> sessionAdditionalServices = new List<SessionAdditionalService>();
+            foreach (var sessionAdditionalService in sessionModel.SessionAdditionalServices)
+            {
+                var additionalServiceExist = cinemaExist.AdditionalServices.FirstOrDefault(service =>
+                    service.Name.Equals(sessionAdditionalService.AdditionalService.Name));
+                sessionAdditionalServices.Add(new SessionAdditionalService()
+                {
+                    AdditionalService = additionalServiceExist,
+                    Price = sessionAdditionalService.Price
+                });
+            }
+
+            List<SessionSeat> sessionSeats = new List<SessionSeat>();
+            foreach (var sessionSeatType in sessionModel.SessionSeatTypes)
+            {
+                sessionSeats.Add(new SessionSeat()
+                {
+                    TicketState = TicketState.Free,
+                    SessionSeatType = _mapper.Map<SessionSeatType>(sessionSeatType)
+                });
+            }
+
+            Session session = new Session
+            {
+                StartDate = sessionModel.StartDate,
+                Movie = movieExist,
+                Hall = hallExist,
+                SessionAdditionalServices = sessionAdditionalServices,
+                SessionSeats = sessionSeats
+            };
+
+            if (!await _sessionRepository.CreateAsync(session))
+            {
+                return new SessionServiceResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        "An error occured while adding to the database"
+                    }
+                };
+            }
+
+            SessionModel newSessionModel = _mapper.Map<SessionModel>(session);
+
+            return new SessionServiceResult()
+            {
+                Success = true,
+                Session = newSessionModel
+            };
         }
 
-        public Task<SessionServiceResult> UpdateSessionInfo(Guid id, SessionModel sessionModel)
+        public Task<SessionServiceResult> UpdateSessionInfo(Guid id, SessionRequestModel sessionModel)
         {
             throw new NotImplementedException();
         }
