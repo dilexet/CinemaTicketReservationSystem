2d69f26660259283f596240395afd65e2cdfd292
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-30 09:19:26 +0300

    Added file service

---
+++ CinemaTicketReservationSystem.BLL/Services/FileService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaTicketReservationSystem.BLL.Abstract.Service;
+using CinemaTicketReservationSystem.BLL.Results.File;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace CinemaTicketReservationSystem.BLL.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public FileService(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public async Task<FileServiceResult> UploadImage(IFormFile file)
+        {
+            try
+            {
+                if (file.Length > 0)
+                {
+                    var directory = "/Files/";
+                    if (!Directory.Exists(_environment.WebRootPath + directory))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + directory);
+                    }
+
+                    var fileType = file.FileName.Split('.').Last();
+                    var path = directory + Guid.NewGuid() + '.' + fileType;
+                    using (FileStream fileStream = new FileStream(_environment.WebRootPath + path, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                        fileStream.Flush();
+                        return new FileServiceResult()
+                        {
+                            Success = true,
+                            PosterPath = path
+                        };
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                return new FileServiceResult()
+                {
+                    Success = false,
+                    Errors = new[]
+                    {
+                        e.Message
+                    }
+                };
+            }
+
+            return new FileServiceResult()
+            {
+                Success = false,
+                Errors = new[]
+                {
+                    "Error while uploading file"
+                }
+            };
+        }
+    }
+}
