3ae183b7e539195c01d50978cffe30e33141683a
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-30 15:03:37 +0300

    Added session controller

--- CinemaTicketReservationSystem.WebApi/Controllers/SessionController.cs
+++ CinemaTicketReservationSystem.WebApi/Controllers/SessionController.cs
@@ -1,6 +1,12 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
 using CinemaTicketReservationSystem.BLL.Abstract.Service;
+using CinemaTicketReservationSystem.BLL.Models.Domain.SessionModels;
+using CinemaTicketReservationSystem.WebApi.Models.Requests.Session;
+using CinemaTicketReservationSystem.WebApi.Models.Response.Session;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CinemaTicketReservationSystem.WebApi.Controllers
@@ -17,6 +23,81 @@ namespace CinemaTicketReservationSystem.WebApi.Controllers
         {
             _sessionService = sessionService;
             _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddSession(SessionRequest sessionRequest)
+        {
+            var result = await _sessionService.AddSession(_mapper.Map<SessionModel>(sessionRequest));
+            var response = _mapper.Map<SessionResponse>(result);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status400BadRequest;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSessionInfo(Guid id, SessionRequest sessionRequest)
+        {
+            var result = await _sessionService.UpdateSessionInfo(id, _mapper.Map<SessionModel>(sessionRequest));
+            var response = _mapper.Map<SessionResponse>(result);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status400BadRequest;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveSession(Guid id)
+        {
+            var result = await _sessionService.RemoveSession(id);
+            var response = _mapper.Map<SessionRemoveResponse>(result);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status400BadRequest;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSessionById(Guid id)
+        {
+            var result = await _sessionService.GetSessionById(id);
+            var response = _mapper.Map<SessionResponse>(result);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status404NotFound;
+                return NotFound(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSessions()
+        {
+            var result = await _sessionService.GetSessions();
+            var response = _mapper.Map<SessionGetAllResponse>(result);
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status404NotFound;
+                return NotFound(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
         }
     }
 }
