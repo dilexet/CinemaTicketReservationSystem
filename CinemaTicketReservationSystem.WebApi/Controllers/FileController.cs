ea55784080faff356f25b40e5a9dc2c093c2a47c
Maksim Pavarha <maksim.pavarha@outlook.com>
2021-12-30 09:21:14 +0300

    Added file controller

---
+++ CinemaTicketReservationSystem.WebApi/Controllers/FileController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using CinemaTicketReservationSystem.BLL.Abstract.Service;
+using CinemaTicketReservationSystem.WebApi.Models.Response.File;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaTicketReservationSystem.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    // [Authorize(Policy = "AdminRole")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IFileService _fileService;
+        private readonly IMapper _mapper;
+
+        public FileController(IFileService fileService, IMapper mapper)
+        {
+            _fileService = fileService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            var result = await _fileService.UploadImage(file);
+            var response = _mapper.Map<FileResponse>(result);
+
+            if (!response.Success)
+            {
+                response.Code = StatusCodes.Status400BadRequest;
+                return BadRequest(response);
+            }
+
+            response.Code = StatusCodes.Status200OK;
+            return Ok(response);
+        }
+    }
+}
