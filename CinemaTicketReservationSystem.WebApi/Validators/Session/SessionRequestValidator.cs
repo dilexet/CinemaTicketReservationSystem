878a87f1caea85213055b063b7c850e5d8512b10
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-03 18:34:34 +0300

    Added check for existence of entity in database

--- CinemaTicketReservationSystem.WebApi/Validators/Session/SessionRequestValidator.cs
+++ CinemaTicketReservationSystem.WebApi/Validators/Session/SessionRequestValidator.cs
@@ -1,14 +1,32 @@
-﻿using CinemaTicketReservationSystem.WebApi.Models.Requests.Session;
+﻿using CinemaTicketReservationSystem.DAL.Abstract;
+using CinemaTicketReservationSystem.WebApi.Extensions;
+using CinemaTicketReservationSystem.WebApi.Models.Requests.Session;
 using FluentValidation;
 
 namespace CinemaTicketReservationSystem.WebApi.Validators.Session
 {
     public class SessionRequestValidator : AbstractValidator<SessionRequest>
     {
-        public SessionRequestValidator()
+        public SessionRequestValidator(
+            IRepository<DAL.Entity.MovieEntity.Movie> movieRepository,
+            IRepository<DAL.Entity.CinemaEntity.Cinema> cinemaRepository,
+            IRepository<DAL.Entity.CinemaEntity.Hall> hallRepository)
         {
+            RuleFor(x => x.MovieName).MovieMustExistAsync(movieRepository);
+            RuleFor(x => x.CinemaName).CinemaMustExistAsync(cinemaRepository);
+
+            RuleFor(x => x)
+                .HallMustExistAsync(cinemaRepository);
+
+            RuleFor(x => x)
+                .AdditionalServicesMustExistAsync(cinemaRepository);
+
+            RuleFor(x => x)
+                .SeatTypesMustExistAsync(hallRepository);
+
             RuleFor(x => x.StartDate).NotNull().NotEmpty().WithMessage("Session start date can't be empty");
             RuleFor(x => x.MovieName).NotEmpty().WithMessage("Movie name can't be empty");
+
             RuleFor(x => x.CinemaName).NotEmpty().WithMessage("Cinema name can't be empty");
             RuleFor(x => x.HallName).NotEmpty().WithMessage("Hall name can't be empty");
 
