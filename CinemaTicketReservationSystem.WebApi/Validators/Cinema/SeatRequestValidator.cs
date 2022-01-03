01c54c26a1aca5f041905a2d118ee657d4feaa2c
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-03 18:35:01 +0300

    Added check for seat types

--- CinemaTicketReservationSystem.WebApi/Validators/Cinema/SeatRequestValidator.cs
+++ CinemaTicketReservationSystem.WebApi/Validators/Cinema/SeatRequestValidator.cs
@@ -1,4 +1,8 @@
-﻿using CinemaTicketReservationSystem.WebApi.Models.Requests.Cinema;
+﻿using System;
+using System.Linq;
+using CinemaTicketReservationSystem.BLL.Enums;
+using CinemaTicketReservationSystem.BLL.Extensions;
+using CinemaTicketReservationSystem.WebApi.Models.Requests.Cinema;
 using FluentValidation;
 
 namespace CinemaTicketReservationSystem.WebApi.Validators.Cinema
@@ -10,6 +14,22 @@ namespace CinemaTicketReservationSystem.WebApi.Validators.Cinema
             RuleFor(x => x.NumberSeat).NotEqual(0U).NotEmpty().WithMessage("Number seat can't be empty or equals 0");
 
             RuleFor(x => x.SeatType).NotEmpty().WithMessage("Seat type can't be empty");
+            RuleFor(x => x.SeatType).Must(seatType =>
+                {
+                    try
+                    {
+                        Enum.GetValues(typeof(SeatTypes))
+                            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                            .Cast<SeatTypes>().First(x => x.GetDisplayName().Equals(seatType));
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        return false;
+                    }
+
+                    return true;
+                })
+                .WithMessage("Seat type is not exist");
         }
     }
 }
