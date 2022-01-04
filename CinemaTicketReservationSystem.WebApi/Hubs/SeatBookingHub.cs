9a8d9052ca7f2073cac14a894170c4b3544ad847
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-04 16:58:42 +0300

    Added Hub

---
+++ CinemaTicketReservationSystem.WebApi/Hubs/SeatBookingHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CinemaTicketReservationSystem.WebApi.Hubs
+{
+    public class SeatBookingHub : Hub
+    {
+        public async Task SetBlockedSeats(Guid[] seatsId)
+        {
+            await Clients.All.SendAsync("setBlockedSeat", seatsId);
+        }
+
+        public async Task SetBookingSeats(Guid[] seatsId)
+        {
+            await Clients.All.SendAsync("setBookingSeat", seatsId);
+        }
+    }
+}
