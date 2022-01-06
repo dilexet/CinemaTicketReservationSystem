5c239806590340d502d590e774ff694274992b8b
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-06 17:51:40 +0300

    Moved to another directory

--- CinemaTicketReservationSystem.WebApi/Extensions/StartupConfigurations/AuthenticationConfigureExtension.cs
+++ CinemaTicketReservationSystem.WebApi/Extensions/StartupConfigurations/AuthenticationConfigureExtension.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 
-namespace CinemaTicketReservationSystem.WebApi.Extensions
+namespace CinemaTicketReservationSystem.WebApi.Extensions.StartupConfigurations
 {
     public static class AuthenticationConfigureExtension
     {
--- CinemaTicketReservationSystem.WebApi/Extensions/StartupConfigurations/SwaggerConfigureExtension.cs
+++ CinemaTicketReservationSystem.WebApi/Extensions/StartupConfigurations/SwaggerConfigureExtension.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
-namespace CinemaTicketReservationSystem.WebApi.Extensions
+namespace CinemaTicketReservationSystem.WebApi.Extensions.StartupConfigurations
 {
     public static class SwaggerConfigureExtension
     {
