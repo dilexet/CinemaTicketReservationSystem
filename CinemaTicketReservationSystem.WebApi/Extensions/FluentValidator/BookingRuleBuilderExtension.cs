8dea7be2d0669b818e6ec007e98607f2af6e801b
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-06 18:50:37 +0300

    Added validation for booking models

---
+++ CinemaTicketReservationSystem.WebApi/Extensions/FluentValidator/BookingRuleBuilderExtension.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using CinemaTicketReservationSystem.DAL.Abstract;
+using CinemaTicketReservationSystem.DAL.Entity.SessionEntity;
+using CinemaTicketReservationSystem.WebApi.Models.Wrappers.Booking;
+using FluentValidation;
+
+namespace CinemaTicketReservationSystem.WebApi.Extensions.FluentValidator
+{
+    public static class BookingRuleBuilderExtension
+    {
+        public static IRuleBuilderOptions<T, BookTicketsRequestWrapper> SessionSeatsMustExistAsync<T>(
+            this IRuleBuilder<T, BookTicketsRequestWrapper> ruleBuilder, IRepository<Session> repository)
+        {
+            var options = ruleBuilder
+                .NotEmpty()
+                .MustAsync(async (bookingRequest, _) =>
+                {
+                    var sessionExist = await repository.FindByIdAsync(bookingRequest.Id);
+                    if (sessionExist == null)
+                    {
+                        return false;
+                    }
+
+                    return bookingRequest.BookTicketsRequest.SessionSeatsId
+                        .All(seatId => sessionExist.SessionSeats
+                            .FirstOrDefault(x => x.Id == seatId) != null);
+                })
+                .WithName("SessionSeat")
+                .WithMessage("Session seat is not exists or booked");
+            return options;
+        }
+
+        public static IRuleBuilderOptions<T, BookTicketsRequestWrapper> SessionAdditionalServicesMustExistAsync<T>(
+            this IRuleBuilder<T, BookTicketsRequestWrapper> ruleBuilder, IRepository<Session> repository)
+        {
+            var options = ruleBuilder
+                .NotEmpty()
+                .MustAsync(async (bookingRequest, _) =>
+                {
+                    var sessionExist = await repository.FindByIdAsync(bookingRequest.Id);
+                    if (sessionExist == null)
+                    {
+                        return false;
+                    }
+
+                    return bookingRequest.BookTicketsRequest.SessionAdditionalServicesId
+                        .All(additionalServiceId => sessionExist.SessionAdditionalServices
+                            .FirstOrDefault(x => x.Id == additionalServiceId) != null);
+                })
+                .WithName("SessionAdditionalService")
+                .WithMessage("Additional service is not exists");
+            return options;
+        }
+    }
+}
---
+++ CinemaTicketReservationSystem.WebApi/Validators/Booking/BookTicketsRequestWrapperValidator.cs
@@ -0,0 +1,21 @@
+﻿using CinemaTicketReservationSystem.DAL.Abstract;
+using CinemaTicketReservationSystem.WebApi.Extensions.FluentValidator;
+using CinemaTicketReservationSystem.WebApi.Models.Wrappers.Booking;
+using FluentValidation;
+
+namespace CinemaTicketReservationSystem.WebApi.Validators.Booking
+{
+    public class BookTicketsRequestWrapperValidator : AbstractValidator<BookTicketsRequestWrapper>
+    {
+        public BookTicketsRequestWrapperValidator(
+            IRepository<DAL.Entity.SessionEntity.Session> sessionRepository,
+            IRepository<DAL.Entity.UserEntity.UserProfile> userProfileRepository)
+        {
+            RuleFor(x => x.Id).SessionMustExistAsync(sessionRepository);
+            RuleFor(x => x.BookTicketsRequest.UserProfileId)
+                .UserProfileMustExistAsync(userProfileRepository);
+            RuleFor(x => x).SessionSeatsMustExistAsync(sessionRepository);
+            RuleFor(x => x).SessionAdditionalServicesMustExistAsync(sessionRepository);
+        }
+    }
+}
