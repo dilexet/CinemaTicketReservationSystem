883cc412d1b37b1504b773e60efd3024bbbcc7f6
Dilexet <dilexet@gmail.com>
2022-01-08 19:37:36 +0300

    Added movie filter parameters model

---
+++ CinemaTicketReservationSystem.BLL/Models/FilterModel/MovieFilterParametersModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CinemaTicketReservationSystem.BLL.Models.FilterModel
+{
+    public class MovieFilterParametersModel
+    {
+        public string MovieName { get; set; }
+
+        public string CinemaName { get; set; }
+
+        public string CityName { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public uint NumberAvailableSeats { get; set; }
+    }
+}
