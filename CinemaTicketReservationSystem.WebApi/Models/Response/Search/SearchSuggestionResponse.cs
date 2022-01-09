eca0027f3f1980de92ce79d120b4ab952fff1f36
Dilexet <dilexet@gmail.com>
2022-01-09 20:19:51 +0300

    Added search suggestion response

---
+++ CinemaTicketReservationSystem.WebApi/Models/Response/Search/SearchSuggestionResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Response.Search
+{
+    public class SearchSuggestionResponse : Response
+    {
+        public IEnumerable<string> ListOfTitles { get; set; }
+    }
+}
