e9733b84439f2130ddff0a35bed4f099e48ebebe
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-19 19:15:10 +0300

    Changed validators

--- CinemaTicketReservationSystem.WebApi/Validators/User/UserCreateRequestValidator.cs
+++ CinemaTicketReservationSystem.WebApi/Validators/User/UserCreateRequestValidator.cs
@@ -11,7 +11,6 @@ namespace CinemaTicketReservationSystem.WebApi.Validators.User
             RuleFor(x => x.Name).Length(5, 16).WithMessage("Username must be between 5 and 16 characters");
 
             RuleFor(x => x.Email).NotEmpty().WithMessage("Please enter the email");
-            RuleFor(x => x.Email).Length(5, 20).WithMessage("Email must be between 5 and 20 characters");
             RuleFor(x => x.Email).EmailAddress().WithMessage("The Email field is not a valid e-mail address");
 
             RuleFor(x => x.RoleName).NotEmpty().WithMessage("Please enter the role name");
