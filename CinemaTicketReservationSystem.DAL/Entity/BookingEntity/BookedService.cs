c158334fea4646ea6f9a250d8cb6194745284736
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-14 14:11:37 +0300

    Added booked service entity

---
+++ CinemaTicketReservationSystem.DAL/Entity/BookingEntity/BookedService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CinemaTicketReservationSystem.DAL.Entity.SessionEntity;
+
+namespace CinemaTicketReservationSystem.DAL.Entity.BookingEntity
+{
+    public class BookedService : BasedEntity
+    {
+        public uint NumberOfServices { get; set; }
+
+        public Guid BookedOrderId { get; set; }
+
+        public Guid SessionAdditionalServiceId { get; set; }
+
+        public virtual BookedOrder BookedOrder { get; set; }
+
+        public virtual SessionAdditionalService SelectedSessionAdditionalService { get; set; }
+    }
+}
