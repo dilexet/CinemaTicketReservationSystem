2ad8ecefa48ce7fd0e4ffcd3147797f0eea27ad8
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-15 16:55:54 +0300

    Added role models

---
+++ CinemaTicketReservationSystem.WebApi/Models/Response/Role/RoleManagementGetRolesResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CinemaTicketReservationSystem.WebApi.Models.ViewModels.User;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.Response.Role
+{
+    public class RoleManagementGetRolesResponse : Response
+    {
+        public IEnumerable<RoleViewModel> Roles { get; set; }
+    }
+}
---
+++ CinemaTicketReservationSystem.WebApi/Models/ViewModels/User/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CinemaTicketReservationSystem.WebApi.Models.ViewModels.User
+{
+    public class RoleViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
