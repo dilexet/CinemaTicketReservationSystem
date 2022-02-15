d2b210905612812e9c3fa3aa313e46b98c65e9ed
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-15 16:55:20 +0300

    Added role service

---
+++ CinemaTicketReservationSystem.BLL/Abstract/Service/IRoleService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CinemaTicketReservationSystem.BLL.Models.Results.Role;
+
+namespace CinemaTicketReservationSystem.BLL.Abstract.Service
+{
+    public interface IRoleService
+    {
+        Task<RoleServiceGetRolesResult> GetRoles();
+    }
+}
